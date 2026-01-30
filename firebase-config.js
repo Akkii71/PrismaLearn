e5f42a89cbb30df846bc4cd7f54685edec6628d6
somraj112 <somrajnandi112@gmail.com>
2026-01-30 22:47:52 +0530

    credentials updated

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Example configuration file.
+// Rename this to firebase-config.js and add your own keys.
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDCYORqElq-Rm6uk30okARLrzkNb27HKY4",
+    authDomain: "Yprismlearn-a737a.firebaseapp.com",
+    projectId: "prismlearn-a737a",
+    storageBucket: "prismlearn-a737a.firebasestorage.app",
+    messagingSenderId: "254286659458",
+    appId: "1:254286659458:web:ad4fa3c7c93cb84604999b"
+};
